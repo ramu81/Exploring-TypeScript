c20db78492fa77e041682efc62a54a76bad61b70
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-10-14 01:19:22 -0400

    Add files via upload

--- generics.js
+++ generics.js
@@ -4,3 +4,23 @@ function identity(arg) {
 console.log(identity("myString"));
 console.log(identity(10));
 console.log(identity(true));
+var List = /** @class */ (function () {
+    function List(val) {
+        this.val = val;
+    }
+    List.prototype.get = function (index) {
+        return this.val[index];
+    };
+    List.prototype.add = function (value) {
+        this.val.push(value);
+    };
+    return List;
+}());
+var emptyNumArr = [];
+var numList = new List(emptyNumArr);
+numList.add(1);
+console.log(numList.get(0));
+var emptyStrArr = [];
+var strList = new List(emptyStrArr);
+strList.add('Kondapalli');
+console.log(strList.get(0));
--- generics.ts
+++ generics.ts
@@ -7,3 +7,27 @@ console.log(identity("myString"));
 console.log(identity(10));
 
 console.log(identity(true));
+
+
+class List<T> {
+
+	constructor(public val: T[]) {}	
+
+	get(index: number): T {
+		return this.val[index];
+	}	
+
+	add(value: T) {
+		this.val.push(value);
+	}
+}
+
+const emptyNumArr: number[] =[];
+const numList = new List<number>(emptyNumArr);
+numList.add(1);
+console.log(numList.get(0));
+
+const emptyStrArr: string[] =[];
+const strList = new List<string>(emptyStrArr);
+strList.add('Kondapalli');
+console.log(strList.get(0));
